2ca7d65da8621268896f261fb115c521838d908e
Sonalib7722 <115664473+Sonalib7722@users.noreply.github.com>
2022-11-06 00:45:59 +0530

    Update CertificationserviceApplicationTests.java

--- src/test/java/com/sb/certifications/CertificationserviceApplicationTests.java
+++ src/test/java/com/sb/certifications/CertificationserviceApplicationTests.java
@@ -3,10 +3,10 @@ package com.sb.certifications;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class CertificationserviceApplicationTests {
 
-
+	@Test
 	void contextLoads() {
 	}
 
